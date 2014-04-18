22d865c77c188412d1679a34a7237e603378c725
chris-maroudas <mallibu97@gmail.com>
2014-04-18 22:40:43 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Particle::Application.config.secret_token = 'e0fc10c72b423c853f2be67121e148e09528aba87d5998fb711e64c40606a1aba11b0c8c5af7f9ba630abdc0d94afab3dbd7f1cb0f9c44d09cb8196f0e0ce0bd'
