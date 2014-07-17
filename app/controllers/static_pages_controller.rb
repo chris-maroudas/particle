669756fa5ccf989b893467839fdcc117d494075e
Chris Maroudas <mallibu97@gmail.com>
2014-07-17 17:55:13 +0300

    removed pg adapter

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,6 @@ class StaticPagesController < ApplicationController
 
   def show
     @static_page = StaticPage.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @static_page }
