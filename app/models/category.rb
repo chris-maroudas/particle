0d632d1b1859c247cbf03eccd93c1a5b679851ec
Chris Maroudas <mallibu97@gmail.com>
2014-08-02 05:36:42 +0300

    Add validations and friendly id to categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -11,5 +11,12 @@
 class Category < ActiveRecord::Base
   attr_accessible :name
 
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
   has_and_belongs_to_many :articles, uniq: true
+
+  validates :name,
+            presence: true,
+            length: { in: 1..60 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140724031148) do
+ActiveRecord::Schema.define(:version => 20140726210548) do
 
   create_table "articles", :force => true do |t|
     t.string   "title"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(:version => 20140724031148) do
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "slug"
   end
 
   create_table "ckeditor_assets", :force => true do |t|
