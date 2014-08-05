466e9fd3e099d7cb1dd9c69c5badd2783c9a95a2
Chris Maroudas <mallibu97@gmail.com>
2014-08-05 03:25:10 +0300

    Implement search functionality on controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,9 @@
 class SearchController < ApplicationController
   def show
+    if params[:query].present?
+      @articles = Article.find_with_index(params[:query], { limit: 10 })
+    else
+      redirect_to root_url, notice: "Please specify a search query!"
+    end
   end
 end
