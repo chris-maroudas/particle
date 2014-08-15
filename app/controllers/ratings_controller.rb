e61f84456d4e49229be209ab05ebe48184723b2c
Chris Maroudas <mallibu97@gmail.com>
2014-08-15 20:21:10 +0300

    Changed alerts to notices in ratings controller

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -7,10 +7,10 @@ class RatingsController < ApplicationController
         @rating.save
         redirect_to Article.find(params[:rating][:article_id]), success: "Comment posted succesfuly"
       else
-        redirect_to Article.find(params[:rating][:article_id]), alert: "Enter a valid rating"
+        redirect_to Article.find(params[:rating][:article_id]), notice: "Enter a valid rating"
       end
     else
-      redirect_to root_url, alert: "You need to be logged in to post a rating!"
+      redirect_to root_url, notice: "You need to be logged in to post a rating!"
     end
   end
 
