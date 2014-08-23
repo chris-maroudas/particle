8962862ce9414644f4b389d702f0d3ab58d518c3
Chris Maroudas <mallibu97@gmail.com>
2014-08-23 19:46:55 +0300

    Changed search controller instance variables

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,8 @@
 class SearchController < ApplicationController
   def show
     if params[:query].present?
-      @articles = Article.find_with_index(params[:query], { limit: 10 })
+      @search_term = params[:query]
+      @articles = Article.find_with_index(@search_term, { limit: 10 })
     else
       redirect_to root_url, notice: "Please specify a search query!"
     end
