a96d3c7d0db740437dfe674add92e3aea10ab8c6
Chris Maroudas <mallibu97@gmail.com>
2014-08-23 19:09:38 +0300

    Add authorization and index action to categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,14 @@
 class CategoriesController < ApplicationController
+
+  load_and_authorize_resource
+
   def show
     @category = Category.find(params[:id])
+    @articles = @category.articles
   end
+
+  def index
+    @categories = Category.all
+  end
+
 end
